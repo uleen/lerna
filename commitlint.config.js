654690b1d201c61b35736a28c813b25c4d9f8aaf
Aleksandr Nikulin <nikuin.aleksandr@cashwagon.com>
2018-10-30 18:34:47 +0300

    feature(FC-1): New title for button

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,13 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        "scope-case": [2, 'always', 'lowerCase'],
+        "scope-case": [2, 'always', 'upper-case'],
         "subject-case": [2, 'always', 'sentence-case'],
         "type-enum": [2, 'always', ['feature', 'fix', 'docs', 'style','test', 'release']]
+    },
+    parserPreset: {
+      parserOpts: {
+        issuePrefixes: ['FC-']
+      }
     }
 };
--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -3,6 +3,6 @@ import * as React from 'react';
 export const Button: React.SFC<{}> = (props): JSX.Element => {
 
     return (
-      <button>Awesome button</button>
+      <button>Awesome button!</button>
     );
 };
