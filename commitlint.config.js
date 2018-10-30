8c47b611c4d2d299158eb97c84a53db579af0851
Aleksandr Nikulin <nikuin.aleksandr@cashwagon.com>
2018-10-30 20:21:20 +0300

    feature(DS): Create new directory

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
+    extends: ['lerna','@commitlint/config-conventional'],
     rules: {
         "scope-case": [2, 'always', 'upper-case'],
         "subject-case": [2, 'always', 'sentence-case'],
