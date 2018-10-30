59d6703342caf795f2b19c3f1a3074a6c00014fd
Aleksandr Nikulin <nikuin.aleksandr@cashwagon.com>
2018-10-30 08:32:31 +0300

    feature(ds-1): New settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
+        "scope-case": [2, 'always', 'lowerCase'],
         "subject-case": [2, 'always', 'sentence-case'],
         "type-enum": [2, 'always', ['feature', 'fix', 'docs', 'style','test', 'release']]
     }
